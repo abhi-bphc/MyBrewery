4a43d717fedcf070494b9d3464d41877964f4548
Abhilash <Abhilash@DESKTOP-UNJFCRG>
2021-01-25 23:57:09 +0530

    First Commit

---
+++ src/main/java/com/Brewery/MyBrewery/MyBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.Brewery.MyBrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Brewery/MyBrewery/MyBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Brewery.MyBrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
